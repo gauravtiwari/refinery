348b0fa833072e5b544c7053e4c113f1a97d130a
Gaurav Tiwari <gaurav@Gauravs-MacBook-Pro.local>
2012-03-16 23:44:01 +0000

    first refinery application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Map::Application.config.secret_token = '17a052805cf6fc71597695f101f9d889fdb24ba53d94951fc9938d14e872124d939d29f41e84c2b38d8064ab9100db1fda6a1c4457c18d34fdff9d98613611aa'
